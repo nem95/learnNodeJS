12474f6b23adc027f956183f828440168c81a9da
Naïm <naiim.chatti@gmail.com>
2020-05-27 16:52:00 +0200

    Creat a mixin for the add store form and adding post controller  route

--- routes/index.js
+++ routes/index.js
@@ -3,5 +3,7 @@ const router = express.Router();
 const storeController = require('../controllers/storeController');
 // Do work here
 router.get('/', storeController.homepage);
+router.get('/add', storeController.addStore);
+router.post('/add', storeController.createStore);
 
 module.exports = router;
