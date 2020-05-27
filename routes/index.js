62c6d5aa600d3ef6ac20c9659d60360f5e5d09cb
Naïm <naiim.chatti@gmail.com>
2020-05-27 18:36:01 +0200

    wrap create store into an error handler and save data to mongodb

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const storeController = require('../controllers/storeController');
-// Do work here
+const { catchErrors } = require('../handlers/errorHandlers');
+
 router.get('/', storeController.homepage);
 router.get('/add', storeController.addStore);
-router.post('/add', storeController.createStore);
+router.post('/add', catchErrors(storeController.createStore));
 
 module.exports = router;
