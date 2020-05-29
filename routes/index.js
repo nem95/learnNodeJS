df51e78c4dec3407a1f656149576eb6d1299b39c
Naïm <naiim.chatti@gmail.com>
2020-05-29 12:38:39 +0200

    Add middleware to be able to add an image to a store

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,17 @@ const storeController = require('../controllers/storeController');
 const { catchErrors } = require('../handlers/errorHandlers');
 
 router.get('/', catchErrors(storeController.getStores));
+router.get('/add', storeController.addStore);
+router.post('/add',
+  storeController.upload,
+  catchErrors(storeController.resize),
+  catchErrors(storeController.createStore)
+);
+router.post('/add/:id',
+  storeController.upload,
+  catchErrors(storeController.resize),
+  catchErrors(storeController.updateStore)
+);
 router.get('/stores', catchErrors(storeController.getStores));
 router.get('/add', storeController.addStore);
 router.post('/add', catchErrors(storeController.createStore));
