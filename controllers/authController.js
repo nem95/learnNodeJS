8d472d595b55a417d50da3d2276a6d19e8dfc350
Naïm <naiim.chatti@gmail.com>
2020-05-31 00:11:33 +0200

    send an email with reset password route url

--- controllers/authController.js
+++ controllers/authController.js
@@ -2,6 +2,7 @@ const passport = require('passport');
 const crypto = require('crypto');
 const mongoose = require('mongoose');
 const { promisify } = require('es6-promisify');
+const mail = require('../handlers/mail');
 
 const User = mongoose.model('User');
 
@@ -40,10 +41,16 @@ exports.forgot = async (req, res) => {
 
   await user.save();
 
-  const resetUrl = `http:/${req.headers.host}/account/reset/${user.resetPasswordToken}`;
+  const resetURL = `http:/${req.headers.host}/account/reset/${user.resetPasswordToken}`;
 
+  await mail.send({
+    user,
+    subject: 'reset password link',
+    resetURL,
+    filename: 'password-reset',
+  });
 
-  req.flash('success', `You have been emailed a password reset link!! ${resetUrl}`);
+  req.flash('success', 'You have been emailed a password reset link!!');
   res.redirect('/login');
 };
 
